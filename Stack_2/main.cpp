e5671170f0342660ed874d815f3f3cd25c62b88c
Võ Hoàng Nhật Khang <khang.vo872003@hcmut.edu.vn>
2022-06-21 10:15:22 +0700

    Update main.cpp

--- Stack_2/main.cpp
+++ Stack_2/main.cpp
@@ -1,3 +1,11 @@
+/*
+    Author: Vo Hoang Nhat Khang
+    Major: Computer Science
+    College: Ho Chi Minh University of Technology
+    Field: Artificial Intelligent chaser
+    21/06/2022
+*/
+
 #include <iostream>
 using namespace std;
 
