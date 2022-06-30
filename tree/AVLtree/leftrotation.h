f35fe990bf4cb5a6e1dbbdc028ad751e35de288a
Võ Hoàng Nhật Khang <khang.vo872003@hcmut.edu.vn>
2022-06-30 16:36:44 +0700

    Create leftrotation.h

---
+++ tree/AVLtree/leftrotation.h
@@ -0,0 +1,9 @@
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = max(height(x->left), height(x->right)) + 1;
+    y->height = max(height(y->left), height(y->right)) + 1;
+    return y;
+}
