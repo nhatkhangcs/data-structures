5e80bfdcd7594a1b6a22b23471a4318998b6f376
Võ Hoàng Nhật Khang <khang.vo872003@hcmut.edu.vn>
2022-06-30 16:38:06 +0700

    Create insertNode.h

---
+++ tree/AVLtree/insertNode.h
@@ -0,0 +1,19 @@
+Node* insert(Node* node, int key) {
+    if (node == NULL) return(newNode(key));
+    if (key < node->key) node->left = insert(node->left, key);
+    else if (key > node->key) node->right = insert(node->right, key);
+    else return node;
+    node->height = 1 + max(height(node->left), height(node->right));
+    int balance = getBalance(node);
+    if (balance > 1 && key < node->left->key) return rightRotate(node);
+    if (balance < -1 && key > node->right->key) return leftRotate(node);
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+    if (balance < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+    return node;
+}
