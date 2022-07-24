c998313c4703d8e711adb901dc581c1ecd22eb56
Võ Hoàng Nhật Khang <khang.vo872003@hcmut.edu.vn>
2022-07-24 10:48:17 +0700

    Update checkBalanceSubtree.h

--- tree/AVLtree/checkBalanceSubtree.h
+++ tree/AVLtree/checkBalanceSubtree.h
@@ -1,4 +1,22 @@
-int getBalance(Node* N) {
-    if (N == NULL) return 0;
-    return height(N->left) - height(N->right);
+bool isBalanced(ptrNode p, int& height){
+    bool avl;
+    if(p == nullptr){
+        avl = true;
+        height = 0;
+    }
+
+    else{
+        int leftHeight, rightHeight;
+        avl = isBalanced(p->left, leftHeight);
+        if(avl) avl = isBalanced(p->right, rightHeight);
+        if(avl){
+            if(leftHeight == rightHeight + 2 || rightHeight == leftHeight + 2) avl = false;
+            else{
+                if(leftHeight > rightHeight) height = leftHeight + 1;
+                else height = rightHeight + 1;
+            }
+        }
+    }
+
+    return avl;
 }
