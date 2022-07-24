f4c3e4e59b7c00063c8f7dd684a00f7c386bc0ff
Võ Hoàng Nhật Khang <khang.vo872003@hcmut.edu.vn>
2022-07-24 10:48:46 +0700

    Update height.h

--- tree/AVLtree/height.h
+++ tree/AVLtree/height.h
@@ -1,7 +1,9 @@
-#include <iostream>
+int getHeight(ptrNode p){
+    if (p == NULL) return 0;
+    else return p->height;
+}
 
-int height(Node* N) {
-    if (N == NULL)
-        return 0;
-    return N->height;
+void ComputeHeight(ptrNode p){
+    if(getHeight(p->left) >= getHeight(p->right)) p->height = getHeight(p->left) + 1;
+    else p->height = getHeight(p->right) + 1;
 }
