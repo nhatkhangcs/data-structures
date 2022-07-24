2e367b77deafa9acf041886b4bf7374d4c52afb8
Võ Hoàng Nhật Khang <khang.vo872003@hcmut.edu.vn>
2022-07-24 10:51:21 +0700

    Update singleRotation.h

--- tree/AVLtree/Left_Rotation/singleRotation.h
+++ tree/AVLtree/Left_Rotation/singleRotation.h
@@ -1,9 +1,8 @@
-Node* leftRotate(Node* x) {
-    Node* y = x->right;
-    Node* T2 = y->left;
-    y->left = x;
-    x->right = T2;
-    x->height = max(height(x->left), height(x->right)) + 1;
-    y->height = max(height(y->left), height(y->right)) + 1;
-    return y;
+void SingleWithLeftChild(ptrNode& p){
+    ptrNode q = p->left;
+    p->left = q->right;
+    q->right = p;
+    ComputeHeight(p);
+    ComputeHeight(q);
+    p = q;
 }
