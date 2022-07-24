30704811f50c4c7b674b3dd3da4b476da6b047a4
Võ Hoàng Nhật Khang <khang.vo872003@hcmut.edu.vn>
2022-07-24 10:49:26 +0700

    Update insertNode.h

--- tree/AVLtree/insertNode.h
+++ tree/AVLtree/insertNode.h
@@ -1,19 +1,24 @@
-Node* insert(Node* node, int key) {
-    if (node == NULL) return(newNode(key));
-    if (key < node->key) node->left = insert(node->left, key);
-    else if (key > node->key) node->right = insert(node->right, key);
-    else return node;
-    node->height = 1 + max(height(node->left), height(node->right));
-    int balance = getBalance(node);
-    if (balance > 1 && key < node->left->key) return rightRotate(node);
-    if (balance < -1 && key > node->right->key) return leftRotate(node);
-    if (balance > 1 && key > node->left->key) {
-        node->left = leftRotate(node->left);
-        return rightRotate(node);
+void Insert(ptrNode& p, int theKey){
+    if(p == nullptr){
+        p = new Node;
+        p->key = theKey;
+        p->height = 1;
+        p->count = 1;
+        p->left = nullptr;
+        p->right = nullptr;
     }
-    if (balance < -1 && key < node->right->key) {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
+
+    else if(theKey < p->key){
+        Insert(p->left, theKey);
+        if(getHeight(p->left) - getHeight(p->right) == 2) RotateWithLeftChild(p);
+        ComputeHeight(p);
     }
-    return node;
+
+    else if(theKey == p->key) p->count = p->count + 1;
+
+    else{
+        Insert(p->right, theKey);
+        if(getHeight(p->right) - getHeight(p->left) == 2) RotateWithRightChild(p);
+        ComputeHeight(p);
+    }
 }
